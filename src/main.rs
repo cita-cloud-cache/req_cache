8ae225b0c8cd02d7904ec9f9f74a5339ac8ebf82
JLer <jlerxky@live.com>
2023-10-10 18:09:54 +0800

    http reverse proxy

--- src/main.rs
+++ src/main.rs
@@ -30,14 +30,11 @@ use std::{net::SocketAddr, sync::Arc};
 
 use anyhow::Result;
 use axum::{
-    extract::State,
-    http::{HeaderMap, StatusCode},
-    middleware,
-    response::IntoResponse,
-    routing::any,
-    Json, Router,
+    extract::State, http::StatusCode, middleware, response::IntoResponse, routing::any, Json,
+    Router,
 };
 use clap::Parser;
+use hyper::{client::HttpConnector, Body, Request, Uri};
 use parking_lot::RwLock;
 use serde_json::json;
 use utoipa::OpenApi;
@@ -47,7 +44,7 @@ use config::Config;
 
 use common_rs::{
     consul::{register_to_consul, ConsulClient},
-    restful::{handle_http_error, ok, RESTfulError},
+    restful::{handle_http_error, RESTfulError},
 };
 
 fn clap_about() -> String {
@@ -97,10 +94,13 @@ fn main() {
 #[openapi(paths(req_filter,), components(schemas()))]
 struct ApiDoc;
 
+type HttpClient = hyper::client::Client<HttpConnector, Body>;
+
 #[derive(Clone)]
 struct AppState {
     _config: Config,
     _consul: Option<Arc<RwLock<ConsulClient>>>,
+    http_client: HttpClient,
 }
 
 #[tokio::main]
@@ -114,6 +114,8 @@ async fn run(opts: RunOpts) -> Result<()> {
         .map_err(|e| println!("tracer init err: {e}"))
         .unwrap();
 
+    let http_client: HttpClient = hyper::Client::builder().build(HttpConnector::new());
+
     let addr = SocketAddr::from(([0, 0, 0, 0], config.port));
 
     let app_state = if let Some(consul_config) = &config.consul_config {
@@ -121,11 +123,13 @@ async fn run(opts: RunOpts) -> Result<()> {
         AppState {
             _config: config,
             _consul: Some(Arc::new(RwLock::new(consul))),
+            http_client,
         }
     } else {
         AppState {
             _config: config,
             _consul: None,
+            http_client,
         }
     };
 
@@ -141,7 +145,7 @@ async fn run(opts: RunOpts) -> Result<()> {
     }
 
     let app = Router::new()
-        .route("/req_cache/api/", any(req_filter))
+        .route("/req_cache/*path", any(req_filter))
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .route_layer(middleware::from_fn(req_logger))
         .route_layer(middleware::from_fn(handle_http_error))
@@ -165,11 +169,12 @@ async fn run(opts: RunOpts) -> Result<()> {
     anyhow::bail!("http server exited!")
 }
 
-#[utoipa::path(post, path = "/req_cache/api/")]
+#[utoipa::path(post, path = "/req_cache/*path")]
 async fn req_filter(
-    headers: HeaderMap,
-    State(_state): State<AppState>,
+    State(state): State<AppState>,
+    mut req: Request<Body>,
 ) -> Result<impl IntoResponse, RESTfulError> {
+    let headers = req.headers();
     debug!("headers: {:?}", headers);
 
     if headers.get("user_code").is_none() {
@@ -181,5 +186,21 @@ async fn req_filter(
 
     // TODO: check user_code and req_id
 
-    ok(true)
+    let path = req.uri().path();
+    let path_query = req
+        .uri()
+        .path_and_query()
+        .map(|v| v.as_str())
+        .unwrap_or(path)
+        .strip_prefix("/req_cache")
+        .unwrap_or(path);
+    debug!("path_query: {}", path_query);
+
+    let uri = format!("http://127.0.0.1{}", path_query);
+
+    *req.uri_mut() = Uri::try_from(uri)?;
+
+    let rsp = state.http_client.request(req).await?;
+
+    Ok(rsp)
 }
